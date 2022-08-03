735fc5b1f7ba19a6748c15faa14c418b3e24c98c
DENİS NAZIM HİKMET GERÇEK <20050111060@ybu.edu.tr>
2022-08-03 16:46:28 +0300

    Modified endgame, added restart method.

--- TicTacToe1.1/Form1.Designer.cs
+++ TicTacToe1.1/Form1.Designer.cs
@@ -29,7 +29,7 @@ namespace TicTacToe1._1
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.c3 = new System.Windows.Forms.Button();
             this.c2 = new System.Windows.Forms.Button();
             this.c1 = new System.Windows.Forms.Button();
@@ -40,35 +40,35 @@ namespace TicTacToe1._1
             this.a2 = new System.Windows.Forms.Button();
             this.a1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tableLayoutPanel
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
-            this.tableLayoutPanel1.Controls.Add(this.c3, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.c2, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.c1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.b3, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.b2, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.b1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.a3, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.a2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.a1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 89);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(225, 195);
-            this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel.Controls.Add(this.c3, 2, 2);
+            this.tableLayoutPanel.Controls.Add(this.c2, 1, 2);
+            this.tableLayoutPanel.Controls.Add(this.c1, 0, 2);
+            this.tableLayoutPanel.Controls.Add(this.b3, 2, 1);
+            this.tableLayoutPanel.Controls.Add(this.b2, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.b1, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.a3, 2, 0);
+            this.tableLayoutPanel.Controls.Add(this.a2, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.a1, 0, 0);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 89);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(225, 195);
+            this.tableLayoutPanel.TabIndex = 2;
             // 
             // c3
             // 
@@ -180,32 +180,34 @@ namespace TicTacToe1._1
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 59);
+            this.label1.Size = new System.Drawing.Size(203, 68);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Your Turn!";
+            this.label1.Text = "X starts.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(225, 284);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tableLayoutPanel);
             this.Controls.Add(this.label1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Tic Tac Toe Game";
-            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.Button c3;
         private System.Windows.Forms.Button c2;
         private System.Windows.Forms.Button c1;
--- TicTacToe1.1/Form1.cs
+++ TicTacToe1.1/Form1.cs
@@ -13,21 +13,24 @@ namespace TicTacToe1._1
     public partial class Form1 : Form
     {
         bool turn = true;
+        int turnCount = 0;
         //x'in sırası true , y'nin sırası false
 
         public Form1()
         {
             InitializeComponent();
+            label1.Enabled = false;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
             Button b = (Button)sender;
             if (turn)
-                b.Text = "x";
+                b.Text = "X";
             else
-                b.Text = "y";
+                b.Text = "O";
             turn = !turn;
+            turnCount++;
             //butona tekrar
             b.Enabled = false;
          
@@ -52,19 +55,56 @@ namespace TicTacToe1._1
             else if ((a3.Text == b2.Text) && (b2.Text == c1.Text) && (!a3.Enabled))
                 winner = true;
             
-
-
-
             if (winner)
             {
+                disableAllButtons();
                 string winner_text = "";
                 if (turn) 
-                winner_text = "Y";
+                winner_text = "O";
                 else
                     winner_text = "X";
-                label1.Text="Winner"+winner_text;
+                label1.Text="Winner is "+winner_text+".\nClick to restart.";
+                label1.Enabled = true;
+            }
+            else if (turnCount==9) {
+                disableAllButtons();
+                label1.Text = "It is a draw.\nClick to restart.";
+                label1.Enabled = true;
+            }
+            else
+            { 
+                if (turn)
+                {
+                    label1.Text = "X's turn.";
+                }
+                else
+                {
+                    label1.Text = "O's turn.";
+                }
             }
         }
-          
+
+        private void disableAllButtons()
+        {
+            foreach(Component c in tableLayoutPanel.Controls)
+            {
+                Button button = (Button)c;
+                button.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            foreach (Component c in tableLayoutPanel.Controls)
+            {
+                Button button = (Button)c;
+                button.Enabled = true;
+                button.Text = "";
+            }
+            label1.Text = "X starts.";
+            label1.Enabled = false;
+            turn = true;
+            turnCount = 0;
+        }
     }
 }
