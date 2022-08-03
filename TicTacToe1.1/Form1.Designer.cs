8aef3231e0dfeef051b849d0bfdc012f01a6139b
4ertan <ertanosmanoglu13@gmail.com>
2022-08-03 16:57:51 +0300

    The End

--- TicTacToe1.1/Form1.Designer.cs
+++ TicTacToe1.1/Form1.Designer.cs
@@ -49,7 +49,7 @@ namespace TicTacToe1._1
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 15F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel.Controls.Add(this.c3, 2, 2);
             this.tableLayoutPanel.Controls.Add(this.c2, 1, 2);
             this.tableLayoutPanel.Controls.Add(this.c1, 0, 2);
@@ -59,25 +59,25 @@ namespace TicTacToe1._1
             this.tableLayoutPanel.Controls.Add(this.a3, 2, 0);
             this.tableLayoutPanel.Controls.Add(this.a2, 1, 0);
             this.tableLayoutPanel.Controls.Add(this.a1, 0, 0);
-            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 89);
-            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 84);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 3;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(225, 195);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(300, 266);
             this.tableLayoutPanel.TabIndex = 2;
             // 
             // c3
             // 
             this.c3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.c3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.c3.Location = new System.Drawing.Point(152, 132);
-            this.c3.Margin = new System.Windows.Forms.Padding(2);
+            this.c3.Location = new System.Drawing.Point(203, 178);
+            this.c3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.c3.Name = "c3";
-            this.c3.Size = new System.Drawing.Size(71, 61);
+            this.c3.Size = new System.Drawing.Size(94, 86);
             this.c3.TabIndex = 8;
             this.c3.UseVisualStyleBackColor = true;
             this.c3.Click += new System.EventHandler(this.button9_Click);
@@ -86,10 +86,10 @@ namespace TicTacToe1._1
             // 
             this.c2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.c2.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.c2.Location = new System.Drawing.Point(77, 132);
-            this.c2.Margin = new System.Windows.Forms.Padding(2);
+            this.c2.Location = new System.Drawing.Point(103, 178);
+            this.c2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.c2.Name = "c2";
-            this.c2.Size = new System.Drawing.Size(71, 61);
+            this.c2.Size = new System.Drawing.Size(94, 86);
             this.c2.TabIndex = 7;
             this.c2.UseVisualStyleBackColor = true;
             this.c2.Click += new System.EventHandler(this.button9_Click);
@@ -98,10 +98,10 @@ namespace TicTacToe1._1
             // 
             this.c1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.c1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.c1.Location = new System.Drawing.Point(2, 132);
-            this.c1.Margin = new System.Windows.Forms.Padding(2);
+            this.c1.Location = new System.Drawing.Point(3, 178);
+            this.c1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.c1.Name = "c1";
-            this.c1.Size = new System.Drawing.Size(71, 61);
+            this.c1.Size = new System.Drawing.Size(94, 86);
             this.c1.TabIndex = 6;
             this.c1.UseVisualStyleBackColor = true;
             this.c1.Click += new System.EventHandler(this.button9_Click);
@@ -110,10 +110,10 @@ namespace TicTacToe1._1
             // 
             this.b3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.b3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.b3.Location = new System.Drawing.Point(152, 67);
-            this.b3.Margin = new System.Windows.Forms.Padding(2);
+            this.b3.Location = new System.Drawing.Point(203, 90);
+            this.b3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.b3.Name = "b3";
-            this.b3.Size = new System.Drawing.Size(71, 61);
+            this.b3.Size = new System.Drawing.Size(94, 84);
             this.b3.TabIndex = 5;
             this.b3.UseVisualStyleBackColor = true;
             this.b3.Click += new System.EventHandler(this.button9_Click);
@@ -122,10 +122,10 @@ namespace TicTacToe1._1
             // 
             this.b2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.b2.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.b2.Location = new System.Drawing.Point(77, 67);
-            this.b2.Margin = new System.Windows.Forms.Padding(2);
+            this.b2.Location = new System.Drawing.Point(103, 90);
+            this.b2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.b2.Name = "b2";
-            this.b2.Size = new System.Drawing.Size(71, 61);
+            this.b2.Size = new System.Drawing.Size(94, 84);
             this.b2.TabIndex = 4;
             this.b2.UseVisualStyleBackColor = true;
             this.b2.Click += new System.EventHandler(this.button9_Click);
@@ -134,10 +134,10 @@ namespace TicTacToe1._1
             // 
             this.b1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.b1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.b1.Location = new System.Drawing.Point(2, 67);
-            this.b1.Margin = new System.Windows.Forms.Padding(2);
+            this.b1.Location = new System.Drawing.Point(3, 90);
+            this.b1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.b1.Name = "b1";
-            this.b1.Size = new System.Drawing.Size(71, 61);
+            this.b1.Size = new System.Drawing.Size(94, 84);
             this.b1.TabIndex = 3;
             this.b1.UseVisualStyleBackColor = true;
             this.b1.Click += new System.EventHandler(this.button9_Click);
@@ -146,10 +146,10 @@ namespace TicTacToe1._1
             // 
             this.a3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.a3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.a3.Location = new System.Drawing.Point(152, 2);
-            this.a3.Margin = new System.Windows.Forms.Padding(2);
+            this.a3.Location = new System.Drawing.Point(203, 2);
+            this.a3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.a3.Name = "a3";
-            this.a3.Size = new System.Drawing.Size(71, 61);
+            this.a3.Size = new System.Drawing.Size(94, 84);
             this.a3.TabIndex = 2;
             this.a3.UseVisualStyleBackColor = true;
             this.a3.Click += new System.EventHandler(this.button9_Click);
@@ -158,10 +158,10 @@ namespace TicTacToe1._1
             // 
             this.a2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.a2.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.a2.Location = new System.Drawing.Point(77, 2);
-            this.a2.Margin = new System.Windows.Forms.Padding(2);
+            this.a2.Location = new System.Drawing.Point(103, 2);
+            this.a2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.a2.Name = "a2";
-            this.a2.Size = new System.Drawing.Size(71, 61);
+            this.a2.Size = new System.Drawing.Size(94, 84);
             this.a2.TabIndex = 1;
             this.a2.UseVisualStyleBackColor = true;
             this.a2.Click += new System.EventHandler(this.button9_Click);
@@ -170,21 +170,21 @@ namespace TicTacToe1._1
             // 
             this.a1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.a1.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.a1.Location = new System.Drawing.Point(2, 2);
-            this.a1.Margin = new System.Windows.Forms.Padding(2);
+            this.a1.Location = new System.Drawing.Point(3, 2);
+            this.a1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.a1.Name = "a1";
-            this.a1.Size = new System.Drawing.Size(71, 61);
+            this.a1.Size = new System.Drawing.Size(94, 84);
             this.a1.TabIndex = 0;
             this.a1.UseVisualStyleBackColor = true;
             this.a1.Click += new System.EventHandler(this.button9_Click);
             // 
             // label1
             // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(11, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 68);
+            this.label1.Size = new System.Drawing.Size(300, 84);
             this.label1.TabIndex = 3;
             this.label1.Text = "X starts.";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -192,12 +192,13 @@ namespace TicTacToe1._1
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(225, 284);
+            this.ClientSize = new System.Drawing.Size(300, 350);
             this.Controls.Add(this.tableLayoutPanel);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(318, 397);
             this.Name = "Form1";
             this.Text = "Tic Tac Toe Game";
             this.tableLayoutPanel.ResumeLayout(false);
